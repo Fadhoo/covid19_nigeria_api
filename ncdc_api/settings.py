21046312fc6e7ab7c88e7ef9a73dd00c9a615ede
Fadipe Al-Ameen <fadipetomi00@gmail.com>
2021-01-15 13:59:58 -0800

    reset db settings

--- ncdc_api/settings.py
+++ ncdc_api/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
     }
 }
 
-DATABASES = {'default': dj_database_url.config(default= config('DATABASE_URL'))}
+# DATABASES = {'default': dj_database_url.config(default= config('DATABASE_URL'))}
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
